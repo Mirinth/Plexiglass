a7469ea181ca6b5242f2514474ab1651e9372eb9
mirinth <Mirinth@users.noreply.github.com>
2021-08-15 15:14:01 -0600

    Handle newlines in Lexer::Lex

--- source/plexlib/lexer.cpp
+++ source/plexlib/lexer.cpp
@@ -48,7 +48,77 @@ void Lexer::Lex()
 	if (m_data.empty())
 	{
 		m_next = Token(m_line, TokenType::Eof, "EOF");
+		return;
 	}
+
+	if (m_data[0] == '\n')
+	{
+		m_next = Token(m_line, TokenType::Newline, "\\n");
+		m_line++;
+		m_data.remove_prefix(1);
+		return;
+	}
+
+	/*
+	* Things to handle:
+keyword expression
+indentifier some-name
+indent
+expression [a-bA-Y09]lyoun(kkol)?
+keyword expression
+identifier another-name
+expression [a-z0-9]{3}
+keyword expression
+identifier third-name
+expression  expression with space
+keyword pattern
+identifier some-name#
+identifier comments
+identifier aren't
+identifier allowed
+identifier at
+identifier the
+identifier end
+identifier of
+identifier the
+identifier line
+indent
+identifier abcde
+indent
+operator |
+identifier fghij
+identifier klmno
+identifier pqrs
+indent
+operator |
+identifier wxyz
+keyword rule
+identifier what-to-match
+indent
+action produce-nothing
+indent
+action produce
+identifier token-name
+indent
+action rewind
+indent
+action transition
+identifier another-state
+indent
+action inc-line
+indent
+action inc-line
+indent
+action dec-line
+indent
+action dec-line
+indent
+action line
+number +2
+indent
+action line
+number -9
+	*/
 
 	if (!m_data.empty())
 	{
