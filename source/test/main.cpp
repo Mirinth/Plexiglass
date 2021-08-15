712315178e4e5524b848899a770a5cd0148bc5d6
mirinth <Mirinth@users.noreply.github.com>
2021-08-15 15:50:03 -0600

    Add basic lexer test harness

--- source/test/main.cpp
+++ source/test/main.cpp
@@ -3,9 +3,10 @@
 
 #include "lexer.hpp"
 
-int main()
+void RunTest()
 {
 	std::ifstream in("../../../tests/lexer/01-in.txt");
+	std::ofstream out("../../../tests/lexer/01-out.txt");
 	std::string data;
 
 	// https://stackoverflow.com/a/2602060
@@ -19,9 +20,45 @@ int main()
 
 	while (lexer.Current().type != TokenType::Eof)
 	{
-		std::cout << lexer.Current() << "\n";
+		out << lexer.Current() << "\n";
 		lexer.Shift();
 	}
+}
 
-	return 0;
+bool CompareOutput()
+{
+	std::ifstream base("../../../tests/lexer/01-in.txt");
+	std::ifstream out("../../../tests/lexer/01-out.txt");
+
+	bool good = true;
+
+	while (base.good() && out.good())
+	{
+		if (base.get() != out.get())
+		{
+			good = false;
+			break;
+		}
+	}
+
+	if (base.eof() && out.eof() && good)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+int main()
+{
+	RunTest();
+	bool success = CompareOutput();
+
+	if (success)
+	{
+		return 0;
+	}
+
+	std::cout << "Files different\n";
+	return 1;
 }
