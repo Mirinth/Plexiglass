e55eb0df968616f07cf9e22bb9a172a86641ab0f
mirinth <Mirinth@users.noreply.github.com>
2021-09-08 19:56:12 -0600

    Handle empty input files.

--- source/plexlib/parser/parser.cpp
+++ source/plexlib/parser/parser.cpp
@@ -1,10 +1,36 @@
 #include <parser/parser.hpp>
 
+#include <sstream>
+
+#include <lexer/lexer.hpp>
+
 ParseException::ParseException(const char* msg)
 	: std::exception(msg)
 {}
 
-void Parse(std::string_view /*data*/)
+void File(Lexer& lexer);
+
+void Error(std::string expected, const Token found);
+
+void Parse(std::string_view data)
 {
-	throw ParseException("Not implemented");
+	Lexer lexer(data);
+	File(lexer);
+}
+
+void File(Lexer& lexer)
+{
+	if (lexer.Peek().type == TokenType::Eof)
+	{
+		Error("keyword", lexer.Peek());
+	}
+}
+
+void Error(std::string expected, const Token tok)
+{
+	std::stringstream out;
+	out << "Syntax error on line " << tok.line
+		<< ": Expected " << expected
+		<< " found " << tok.ToString();
+	throw ParseException(out.str().c_str());
 }
