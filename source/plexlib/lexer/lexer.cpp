d919664255a48685b7408dca29d6e44e4310481f
mirinth <Mirinth@users.noreply.github.com>
2021-09-11 17:16:36 -0600

    Detect duplicate | in pattern statements.

--- source/plexlib/lexer/lexer.cpp
+++ source/plexlib/lexer/lexer.cpp
@@ -127,13 +127,14 @@ void Lexer::LexLine(std::string_view line)
 		while (!IsBlank(line))
 		{
 			line = StripWhitespace(line);
-			std::string text = LexToken(line);
-			if (text == "|")
+			if (line[0] == '|')
 			{
-				m_buffer.push(Token(m_lineNumber, TokenType::Alternator, text));
+				m_buffer.push(Token(m_lineNumber, TokenType::Alternator, "|"));
+				line.remove_prefix(1);
 			}
 			else
 			{
+				std::string text = LexToken(line);
 				m_buffer.push(Token(m_lineNumber, TokenType::Text, text));
 			}
 		}
