ed2ed7798a0b3e5058860e305f9e67e96bed5c1d
mirinth <Mirinth@users.noreply.github.com>
2021-09-19 17:31:18 -0600

    Initial lexer skeleton.

---
+++ data/skeleton.cpp
@@ -0,0 +1,79 @@
+#include "$LEXER_NAME.hpp"
+
+#include <regex>
+#include <tuple>
+#include <vector>
+
+std::string ReadFile(std::string path);
+
+$LEXER_NAME::$LEXER_NAME(istream& input)
+{
+    m_input = ReadFile(path);
+    m_data = m_input;
+    Shift();
+}
+
+TokenType $LEXER_NAME::PeekToken() const
+{
+    return m_type;
+}
+
+std::string &LEXER_NAME::PeekText() const
+{
+    return m_text;
+}
+
+void &LEXER_NAME::Shift()
+{
+    static std::vector<std::tuple<TokenType, std::regex>> rules = {
+        $LEXER_RULES
+    };
+
+    unsigned int max_index = 0;
+    unsigned int max_length = 0;
+    std::string max_string;
+
+    for (size_t index = 0; index < rules.length(); index++)
+    {
+        auto& [type, regex] = rules[index];
+        std::match_results m;
+        bool matched = std::regex_search(m_data.begin(), m_data.end(), m, regex);
+        if (!matched)
+        {
+            continue;
+        }
+
+        if (m.length() > max_length)
+        {
+            max_length = m.length();
+            max_index = index;
+            max_string = m.str();
+        }
+    }
+
+    if (max_length > 0)
+    {
+        m_type = rules[max_index].first();
+        m_text = m_data.substr(0, max_length);
+        m_data.remove_prefix(max_length);
+    }
+    else
+    {
+        m_type = $INVALID_TOKEN;
+        m_text = "";
+        m_data.remove_prefix(1);
+    }
+}
+
+std::string ReadFile(std::string path)
+{
+    std::ifstream in(path);
+    std::string data;
+
+    auto fileSize = std::filesystem::file_size(path);
+    data.reserve(fileSize);
+    data.assign(std::istreambuf_iterator<char>(in),
+                std::istreambuf_iterator<char>());
+
+    return data;
+}
---
+++ data/skeleton.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+
+enum TokenType
+{
+    $TOKEN_NAMES
+};
+
+class $LEXER_NAME
+{
+public:
+    $LEXER_NAME(std::string path);
+    TokenType PeekToken() const;
+    std::string PeekText() const;
+    void Shift();
+
+private:
+    std::string m_input;
+    std::string_view m_data;
+    TokenType m_type;
+    std::string m_text;
+};
