357bd96620d270d34b1084acca83843949c44b43
mirinth <Mirinth@users.noreply.github.com>
2021-09-19 11:31:57 -0600

    Silence more stdlib warnings.

--- source/test/main.cpp
+++ source/test/main.cpp
@@ -1,9 +1,9 @@
 #include <std/filesystem>
 #include <std/fstream>
-#include <functional>
-#include <iostream>
-#include <string>
-#include <vector>
+#include <std/functional>
+#include <std/iostream>
+#include <std/string>
+#include <std/vector>
 
 #include <error.hpp>
 #include <analyzer/analyzer.hpp>
