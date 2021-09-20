09170d42b22b18471c833d94fe0f89929d9224d7
mirinth <Mirinth@users.noreply.github.com>
2021-09-19 19:31:30 -0600

    Fill in lexer name in header.

--- source/plexlib/template/template.cpp
+++ source/plexlib/template/template.cpp
@@ -1,10 +1,31 @@
 #include <template/template.hpp>
 
+#include <algorithm>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 
 std::string ReadFile(std::string path);
+
+// https://stackoverflow.com/a/3418285
+void Replace(std::string& subject,
+             const std::string& find,
+             const std::string& replace)
+{
+    if (find.empty())
+    {
+        return;
+    }
+
+    size_t start_pos = 0;
+    while ((start_pos = subject.find(find, start_pos)) != std::string::npos)
+    {
+        subject.replace(start_pos, find.length(), replace);
+        start_pos += replace.length(); // In case 'replace' contains 'find',
+                                       // like replacing 'x' with 'yx'
+    }
+}
+
 
 void TemplateHeader(FileNode /*file*/, std::string dir, std::string name)
 {
@@ -12,6 +33,8 @@ void TemplateHeader(FileNode /*file*/, std::string dir, std::string name)
     headerPath.replace_extension(".hpp");
 
     std::string headerContent = ReadFile("template.hpp");
+
+    Replace(headerContent, "$LEXER_NAME", name);
 
     std::ofstream out(headerPath);
     out << headerContent;
