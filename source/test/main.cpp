ae3a74d30e8c66beee6970fe1578c9b58d6d3166
mirinth <Mirinth@users.noreply.github.com>
2021-10-03 17:38:36 -0600

    Move lexer test into its own file.

--- source/test/main.cpp
+++ source/test/main.cpp
@@ -200,23 +200,6 @@ bool TestGroup(std::string name, Tester test)
     return true;
 }
 
-TEST_CASE("Test the lexer")
-{
-    std::stringstream out;
-    std::string input = ReadTestFile("lexer/lexer-test-in.txt");
-    std::string base = ReadTestFile("lexer/lexer-test-base.txt");
-    
-    Lexer lexer(input);
-
-    while (lexer.Peek().type != TokenType::Eof)
-    {
-        out << lexer.Peek().ToString() << "\n";
-        lexer.Shift();
-    }
-
-    CHECK(base == out.str());
-}
-
 //int main()
 //{
 //    std::vector<std::tuple<std::string, Tester>> map = {
