442791e8c9908d78b85ecea1bc106812d53da768
mirinth <Mirinth@users.noreply.github.com>
2021-10-02 20:48:11 -0600

    Validate command line parameters.

--- source/plexi/main.cpp
+++ source/plexi/main.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
+#include <string>
 
 void PrintUsage()
 {
     std::cout
         << "Usage:\n"
-        << "plexiglass filename\n"
+        << "    plexiglass filename\n"
         << "\n"
         << "  filename: Name of the input file.\n"
         << "\n"
         << "Note: Output file and lexer name are based on input file name.\n";
 }
 
-int main()
+int main(int argc, const char** argv)
 {
-    PrintUsage();
+    if (argc != 2)
+    {
+        PrintUsage();
+        return 1;
+    }
+
+    argc--; // discard executable name
+    argv++;
+
+    std::string filename = argv[0];
+
+    if (filename == "-h")
+    {
+        PrintUsage();
+        return 1;
+    }
+
+    std::cout << "Plexiglass is running\n";
+
 	return 0;
 }
