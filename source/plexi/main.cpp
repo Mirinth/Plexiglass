05932dc621592463182642bb3c5312ea60b21d05
mirinth <Mirinth@users.noreply.github.com>
2021-10-03 11:36:52 -0600

    Run the lexer generator.

--- source/plexi/main.cpp
+++ source/plexi/main.cpp
@@ -2,6 +2,13 @@
 #include <iostream>
 #include <string>
 
+#include <analyzer/analyzer.hpp>
+#include <error.hpp>
+#include <parser/parser.hpp>
+#include <parser/tree.hpp>
+#include <template/template.hpp>
+#include <utils.hpp>
+
 void PrintUsage()
 {
     std::cout
@@ -37,10 +44,34 @@ int main(int argc, const char** argv)
     std::filesystem::path code = source.replace_extension(".cpp");
     std::string lexerName = source.filename().stem().string();
     
-    std::cout << "Filename: " << source << "\n";
-    std::cout << "Lexer name: " << lexerName << "\n";
-    std::cout << "Header name: " << header << "\n";
-    std::cout << "Code name: " << code << "\n";
-
-    return 0;
+    try
+    {
+        std::string data = ReadFile(source.string());
+        FileNode file = Parse(data);
+        Analyze(file);
+        Template(file, lexerName, header.string(), code.string());
+        return 0;
+    }
+    catch (const PlexiException& exc)
+    {
+        std::cerr << "Generation failed: " << exc.what() << std::endl;
+        return 2;
+    }
+    catch (const std::filesystem::filesystem_error&)
+    {
+        // source can be inserted directly, but it prints an escaped string,
+        // which might mislead users into thinking the problem is redundant
+        // backslashes.
+        std::cerr << "Unable to read file " << source.string() << std::endl;
+        return 3;
+    }
+    catch (const std::exception& exc)
+    {
+        std::cerr << "An unhandled exception occurred: " << exc.what()
+                  << std::endl;
+    }
+    catch (...)
+    {
+        std::cerr << "An unrecognized exception was thrown.\n";
+    }
 }
