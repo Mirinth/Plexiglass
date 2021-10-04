eace34ba6c0f6c6781374243e59617164bebc15f
mirinth <Mirinth@users.noreply.github.com>
2021-10-04 16:20:45 -0600

    Add test case for help parameter.

--- source/test/test_parameters.cpp
+++ source/test/test_parameters.cpp
@@ -15,3 +15,15 @@ TEST_CASE("Parameters: No parameters")
     CHECK(out.str() == base.str());
     CHECK(err.str() == "");
 }
+
+TEST_CASE("Parameters: -h")
+{
+    std::stringstream out, err, base;
+    std::vector<std::string> params = { "-h" };
+
+    PrintUsage(base);
+    PlexMain(params, out, err);
+
+    CHECK(out.str() == base.str());
+    CHECK(err.str() == "");
+}
