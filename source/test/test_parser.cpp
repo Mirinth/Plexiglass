556c9e3fda3a6725e8c5ba68c21855cd0d04d6bb
mirinth <Mirinth@users.noreply.github.com>
2021-10-03 18:07:51 -0600

    Convert remaining parser 1xx tests.

--- source/test/test_parser.cpp
+++ source/test/test_parser.cpp
@@ -23,10 +23,34 @@ TEST_CASE("Parser: No keyword at start")
                          PlexiException);
 }
 
-TEST_CASE("Parser: Expression with no identifier.")
+TEST_CASE("Parser: Expression with no identifier")
 {
     std::string data = ReadTestFile("parser/expression-with-no-identifier.txt");
     CHECK_THROWS_WITH_AS(Parse(data),
                          "Error on line 2: Expected identifier found eof",
+                         PlexiException);
+}
+
+TEST_CASE("Parser: Expression with no indent")
+{
+    std::string data = ReadTestFile("parser/expression-with-no-indent.txt");
+    CHECK_THROWS_WITH_AS(Parse(data),
+                         "Error on line 2: Expected indent found eof",
+                         PlexiException);
+}
+
+TEST_CASE("Parser: Expression with invalid regex")
+{
+    std::string data = ReadTestFile("parser/expression-with-bad-regex.txt");
+    CHECK_THROWS_WITH_AS(Parse(data),
+                         "Error on line 3: Malformed regex.",
+                         PlexiException);
+}
+
+TEST_CASE("Parser: Expression with extra stuff on first line")
+{
+    std::string data = ReadTestFile("parser/expression-long-first-line.txt");
+    CHECK_THROWS_WITH_AS(Parse(data),
+                         "Error on line 2: Expected indent found text junk",
                          PlexiException);
 }
