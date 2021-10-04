9591a69b0148e4bbc7f1735f90dc5f20706a85a1
mirinth <Mirinth@users.noreply.github.com>
2021-10-03 18:02:03 -0600

    Convert parser test case 101.

--- source/test/test_parser.cpp
+++ source/test/test_parser.cpp
@@ -9,7 +9,7 @@
 
 TEST_CASE("Parser: Empty file")
 {
-    std::string data = ReadTestFile("parser/empty-file-in.txt");
+    std::string data = ReadTestFile("parser/empty-file.txt");
     CHECK_THROWS_WITH_AS(Parse(data),
                          "Error on line 1: Expected keyword found eof",
                          PlexiException);
@@ -20,5 +20,13 @@ TEST_CASE("Parser: No keyword at start")
     std::string data = ReadTestFile("parser/no-keyword-at-start.txt");
     CHECK_THROWS_WITH_AS(Parse(data),
                          "Error on line 2: Unrecognized keyword junk",
+                         PlexiException);
+}
+
+TEST_CASE("Parser: Expression with no identifier.")
+{
+    std::string data = ReadTestFile("parser/expression-with-no-identifier.txt");
+    CHECK_THROWS_WITH_AS(Parse(data),
+                         "Error on line 2: Expected identifier found eof",
                          PlexiException);
 }
