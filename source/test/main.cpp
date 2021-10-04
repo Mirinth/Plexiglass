06503f548ded99b664659dab60ff995142ba2cb4
mirinth <Mirinth@users.noreply.github.com>
2021-10-03 22:27:59 -0600

    Comment ignored warning on doctest.h include.

--- source/test/main.cpp
+++ source/test/main.cpp
@@ -3,6 +3,7 @@
 #include <utils.hpp>
 
 #pragma warning(push)
+// unreferenced function with internal linkage removed
 #pragma warning(disable : 5245)
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
