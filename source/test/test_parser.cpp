1817e96b0dde297d80d5ec445bf7ce3bc7d63eba
mirinth <Mirinth@users.noreply.github.com>
2021-10-03 18:19:45 -0600

    Convert parser 2xx test cases.

--- source/test/test_parser.cpp
+++ source/test/test_parser.cpp
@@ -7,6 +7,10 @@
 
 #include "read_test_file.hpp"
 
+// ============================================================================
+// Misc test cases
+// ============================================================================
+
 TEST_CASE("Parser: Empty file")
 {
     std::string data = ReadTestFile("parser/empty-file.txt");
@@ -22,6 +26,10 @@ TEST_CASE("Parser: No keyword at start")
                          "Error on line 2: Unrecognized keyword junk",
                          PlexiException);
 }
+
+// ============================================================================
+// Expression test cases
+// ============================================================================
 
 TEST_CASE("Parser: Expression with no identifier")
 {
@@ -42,8 +50,7 @@ TEST_CASE("Parser: Expression with no indent")
 TEST_CASE("Parser: Expression with invalid regex")
 {
     std::string data = ReadTestFile("parser/expression-with-bad-regex.txt");
-    CHECK_THROWS_WITH_AS(Parse(data),
-                         "Error on line 3: Malformed regex.",
+    CHECK_THROWS_WITH_AS(Parse(data), "Error on line 3: Malformed regex.",
                          PlexiException);
 }
 
@@ -53,4 +60,72 @@ TEST_CASE("Parser: Expression with extra stuff on first line")
     CHECK_THROWS_WITH_AS(Parse(data),
                          "Error on line 2: Expected indent found text junk",
                          PlexiException);
+}
+
+// ============================================================================
+// Rule test cases
+// ============================================================================
+
+TEST_CASE("Parser: Rule with no identifier")
+{
+    std::string data = ReadTestFile("parser/rule-with-no-identifier.txt");
+    CHECK_THROWS_WITH_AS(Parse(data),
+                         "Error on line 2: Expected identifier found eof",
+                         PlexiException);
+}
+
+TEST_CASE("Parser: Rule with no action")
+{
+    std::string data = ReadTestFile("parser/rule-with-no-action.txt");
+    CHECK_THROWS_WITH_AS(Parse(data),
+                         "Error on line 2: Expected indent found eof",
+                         PlexiException);
+}
+
+TEST_CASE("Parser: Rule with extra stuff on first line")
+{
+    std::string data = ReadTestFile("parser/rule-long-first-line.txt");
+    CHECK_THROWS_WITH_AS(Parse(data),
+                         "Error on line 2: Expected indent found text junk",
+                         PlexiException);
+}
+
+TEST_CASE("Parser: Rule with bad action")
+{
+    std::string data = ReadTestFile("parser/rule-with-bad-action.txt");
+    CHECK_THROWS_WITH_AS(Parse(data),
+                         "Error on line 3: Expected action found text junk",
+                         PlexiException);
+}
+
+TEST_CASE("Parser: Rule with produce at end of file")
+{
+    std::string data = ReadTestFile("parser/rule-with-produce-at-eof.txt");
+    CHECK_THROWS_WITH_AS(
+        Parse(data), "Error on line 3: Expected identifier before end of line",
+        PlexiException);
+}
+
+TEST_CASE("Parser: Rule with empty produce")
+{
+    std::string data = ReadTestFile("parser/rule-with-empty-produce.txt");
+    CHECK_THROWS_WITH_AS(
+        Parse(data), "Error on line 3: Expected identifier before end of line",
+        PlexiException);
+}
+
+TEST_CASE("Parser: Rule with transition at eof")
+{
+    std::string data = ReadTestFile("parser/rule-with-transition-at-eof.txt");
+    CHECK_THROWS_WITH_AS(
+        Parse(data), "Error on line 3: Expected identifier before end of line",
+        PlexiException);
+}
+
+TEST_CASE("Parser: Rule with empty transition")
+{
+    std::string data = ReadTestFile("parser/rule-with-empty-transition.txt");
+    CHECK_THROWS_WITH_AS(
+        Parse(data), "Error on line 3: Expected identifier before end of line",
+        PlexiException);
 }
