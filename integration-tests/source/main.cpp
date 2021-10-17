d63e338726baa17de622877b0097fe180d8af7dc
mirinth <Mirinth@users.noreply.github.com>
2021-10-16 21:10:25 -0600

    Make lexer load correct input file.

--- integration-tests/source/main.cpp
+++ integration-tests/source/main.cpp
@@ -2,8 +2,16 @@
 
 #include <lexer.hpp>
 
+#include <path.hpp>
+
 int main()
 {
+    std::string basePath = base_path;
+    std::string lexerPath = basePath + "lexer.txt";
+    lexer lex(lexerPath);
+
+
+
     std::cout << "Integration test\n";
     return 0;
 }
---
+++ integration-tests/source/path.hpp
@@ -0,0 +1,1 @@
+const char* base_path = "${LEXER_FILE_PATH}/";
