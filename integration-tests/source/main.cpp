3d4e7852b969838fcf40f151c337a612d3b80342
mirinth <Mirinth@users.noreply.github.com>
2021-10-19 20:58:23 -0600

    Check if integration test actually passed.

--- integration-tests/source/main.cpp
+++ integration-tests/source/main.cpp
@@ -1,15 +1,14 @@
 #include <fstream>
 #include <iostream>
 
-#include <lexer.hpp>
-
 #include <path.hpp>
 
-int main()
+#include <lexer.hpp>
+
+std::string ReadFile(std::string path);
+
+void RunLexer(std::string inputPath, std::string outputPath)
 {
-    std::string basePath = base_path;
-    std::string inputPath = basePath + "input.txt";
-    std::string outputPath = basePath + "out.txt";
     lexer lex(inputPath);
 
     std::ofstream out(outputPath);
@@ -21,6 +20,27 @@ int main()
     }
 
     out << ToString(lex.PeekToken()) << " " << lex.PeekText() << "\n";
+}
 
-    return 0;
+int main()
+{
+    std::string basePath = base_path;
+    std::string inputPath = basePath + "input.txt";
+    std::string outputPath = basePath + "out.txt";
+
+    RunLexer(inputPath, outputPath);
+    
+    std::string base = ReadFile(basePath);
+    std::string out = ReadFile(outputPath);
+
+    if (base == out)
+    {
+        std::cout << "Pass\n";
+        return 0;
+    }
+    else
+    {
+        std::cout << "Fail\n";
+        return 1;
+    }
 }
