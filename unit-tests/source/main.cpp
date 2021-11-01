19f58785a596e80a6b9acca2d3b6566aabb8d47d
mirinth <Mirinth@users.noreply.github.com>
2021-10-31 23:20:07 -0600

    Add documentation comments to unit-test main.cpp.

--- unit-tests/source/main.cpp
+++ unit-tests/source/main.cpp
@@ -6,12 +6,24 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
+/// <summary>
+/// Gets an absolute path to the folder where the test files are located.
+/// Assumes the current directory is where test files are located.
+/// </summary>
+/// <returns>Absolute path to test files folder.</returns>
 std::string GetTestRoot()
 {
     auto root = std::filesystem::absolute("./");
     return root.string();
 }
 
+/// <summary>
+/// Read the contents of a test file in as a string.
+/// </summary>
+/// <param name="name">
+/// Path to the test file, relative to the current directory.
+/// </param>
+/// <returns>The contents of the test file.</returns>
 std::string ReadTestFile(std::string name)
 {
     std::string path = GetTestRoot() + name;
