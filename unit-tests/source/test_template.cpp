bb8fd38ea06e587362579258073e99ae9f830ca6
mirinth <Mirinth@users.noreply.github.com>
2021-11-27 17:14:10 -0700

    Refactor template tests.

--- unit-tests/source/test_template.cpp
+++ unit-tests/source/test_template.cpp
@@ -9,46 +9,34 @@
 
 #include "test_files.hpp"
 
-TEST_CASE("Template: Test template")
+void TemplateTest(std::string name, bool debug)
 {
     std::filesystem::path testDir = GetTestRoot() / "template/";
 
-    std::string testName = "simple";
-    std::filesystem::path header = testDir / "simple-out.hpp";
-    std::filesystem::path code = testDir / "simple-out.cpp";
-    std::filesystem::path source = testDir / "simple-in.txt";
+    std::string testName = name;
+    std::filesystem::path header = testDir / (name + "-out.hpp");
+    std::filesystem::path code = testDir / (name + "-out.cpp");
+    std::filesystem::path source = testDir / (name + "-in.txt");
 
     FileNode file = Parse(source);
     Analyze(file);
-    Template(file, testName, header, code, false);
+    Template(file, testName, header, code, debug);
 
-    std::string base = ReadTestFile("template/simple-base.hpp");
-    std::string out = ReadTestFile("template/simple-out.hpp");
+    std::string base = ReadTestFile("template/" + name + "-base.hpp");
+    std::string out = ReadTestFile("template/" + name + "-out.hpp");
     CHECK(base == out);
 
-    base = ReadTestFile("template/simple-base.cpp");
-    out = ReadTestFile("template/simple-out.cpp");
+    base = ReadTestFile("template/" + name + "-base.cpp");
+    out = ReadTestFile("template/" + name + "-out.cpp");
     CHECK(base == out);
+}
+
+TEST_CASE("Template: Test template")
+{
+    TemplateTest("simple", false);
 }
 
 TEST_CASE("Template: Test template with debug driver")
 {
-    std::filesystem::path testDir = GetTestRoot() / "template/";
-
-    std::string testName = "debug";
-    std::filesystem::path header = testDir / "debug-out.hpp";
-    std::filesystem::path code = testDir / "debug-out.cpp";
-    std::filesystem::path source = testDir / "debug-in.txt";
-
-    FileNode file = Parse(source);
-    Analyze(file);
-    Template(file, testName, header, code, true);
-
-    std::string base = ReadTestFile("template/debug-base.hpp");
-    std::string out = ReadTestFile("template/debug-out.hpp");
-    CHECK(base == out);
-
-    base = ReadTestFile("template/debug-base.cpp");
-    out = ReadTestFile("template/debug-out.cpp");
-    CHECK(base == out);
+    TemplateTest("debug", true);
 }
